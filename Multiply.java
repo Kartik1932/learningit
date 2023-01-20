93d405a258c41bf4ba58e4c2d329c9d4db545f9c
Kartik <kartiknangla882@gmail.com>
2023-01-20 16:04:20 +0530

    Added multiply in sample branch

---
+++ Multiply.java
@@ -0,0 +1,7 @@
+public class Multiply{
+    public static void main(String[] args){
+        int a = 4;
+        int b = 3;
+        System.out.println("The product of the two numbers is : "+(a*b));
+    }
+}
