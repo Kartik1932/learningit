bc37cca8c400b5e5e29d52539bc9f7df6d157580
Kartik <kartiknangla882@gmail.com>
2023-01-20 14:50:08 +0530

    third commit(added Diff.java)

---
+++ Diff.java
@@ -0,0 +1,8 @@
+public class Diff{
+    public static void main(String[] args){
+        int a = 55;
+        int b = 43;
+
+        System.out.println("The difference between a and b is ", a - b);
+    }
+}
